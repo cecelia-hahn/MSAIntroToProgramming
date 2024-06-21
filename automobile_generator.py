dda263564ae7e08e78eb1837b05a1a977a3cd00e
cecelia-hahn <ccrhahn17@gmail.com>
2024-06-21 09:51:40 -0500

    Add files via upload

--- automobile_generator.py
+++ automobile_generator.py
@@ -5,5 +5,21 @@ def main():
     auto1 = automobile.Automobile("Ford", "Focus", "1234", 2.2, "Alice", 2013)
     auto2 = automobile.Automobile("Honda", "Accord", "23456", 3.0, "Bob", 2017)
 
-    print(f"{auto1.make} {auto1.model}: {auto1.year}")
+    #print data for auto1 and auto2
+    auto1.print_info()
+    auto2.print_info()
+
+    #change auto2 engine size
+    auto2.set_engine_size(2.4)
+
+    #change auto1 owner
+    auto1.set_owner("Syd")
+
+    auto1.print_info()
+    auto2.print_info()
+
+    #print car age info
+    print(f"Auto1 is {auto1.get_age()} years old")
+    print(f"Auto2 is {auto2.get_age()} years old")
+
 main()
